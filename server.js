70ce9110856d52bc77021276df941f974270ac15
Ivan <ivan.baena04@gmail.com>
2016-03-13 13:14:09 -0400

    set up server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    app = express();
+
+var PORT = process.env.PORT || 8000;
+
+//DEPENDENCIES
+var exphbs = require('express-handlebars'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    cheerio = require('cheerio');
+    mongoose = require('mongoose');
+
+//SET HANDLEBARS ENGINE
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//BODYPARSER TO READ HTML
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// SETUP LOGGER
+app.use(morgan('dev'));
+
+//SERVE FILES FROM PUBLIC DIR
+app.use('/public', express.static(__dirname + "/public"));
+
+
+app.listen(PORT, function() {
+  console.log("Listening on PORT %s", PORT);
+});
